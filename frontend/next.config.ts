bbe54f368e61c8f803db5403724dbb114d32894c
bharatesh <bharatesh.kamaj@doselect.com>
2026-04-08 01:03:24 +0530

    base code 005

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   transpilePackages: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
   reactStrictMode: true,
 };
